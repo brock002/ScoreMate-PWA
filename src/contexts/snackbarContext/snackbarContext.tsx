62970331d1fd5944232727a47f1aa2532593ebdd
BiswajitDeka <utpaldeka102@gmail.com>
2024-03-26 23:12:57 +0530

    minor fix

--- src/contexts/snackbarContext/snackbarContext.tsx
+++ src/contexts/snackbarContext/snackbarContext.tsx
@@ -1,78 +1,70 @@
-import { Slide, SlideProps, Snackbar } from '@mui/material'
-import React, { createContext, useContext, useState } from 'react'
-import { SnackbarStateType, SnackbarContextType } from './snackbarContext.types'
-
+import { Slide, SlideProps, Snackbar } from "@mui/material"
+import React, { createContext, useContext, useState } from "react"
+import { SnackbarStateType, SnackbarContextType } from "./snackbarContext.types"
 
 // BROKEN
 // DO NOT USE
 
-
 // creating context
 const SnackbarContext = createContext<SnackbarContextType>({
-	enqueueSnackbar: () => { },
-	closeSnackbar: () => { },
+    enqueueSnackbar: () => {},
+    closeSnackbar: () => {},
 })
 
 // defining transition component for snackbar
 const SlideTransition = (props: SlideProps) => (
-	<Slide {...props} direction='up' />
+    <Slide {...props} direction="up" />
 )
-
-// defining initial state for snackbar state
-const INITIAL_STATE = {
-	message: '',
-	autoHideDuration: 5000,
-}
 
 // creating context provider
 const SnackbarContextProvider: React.FC<{
-	children: React.ReactNode
+    children: React.ReactNode
 }> = (props) => {
-	const [isOpen, setIsOpen] = useState<boolean>(false)
-	const [state, setState] = useState<SnackbarStateType>()
-	const [currentSnackBarIndex, setCurrentSnackBarIndex] = useState(0)
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [state, setState] = useState<SnackbarStateType>()
+    const [currentSnackBarIndex, setCurrentSnackBarIndex] = useState(0)
 
-	// defining function to show snackbar
-	const enqueueSnackbar = (props: SnackbarStateType) => {
-		setCurrentSnackBarIndex((prev) => prev + 1)
-		setIsOpen(true)
-		setState(props)
-	}
+    // defining function to show snackbar
+    const enqueueSnackbar = (props: SnackbarStateType) => {
+        setCurrentSnackBarIndex((prev) => prev + 1)
+        setIsOpen(true)
+        setState(props)
+    }
 
-	// defining function to hide snackbar
-	const handleClose = () => {
-		setIsOpen(false)
-		// setState(INITIAL_STATE)
-	}
+    // defining function to hide snackbar
+    const handleClose = () => {
+        setIsOpen(false)
+        // setState(INITIAL_STATE)
+    }
 
-	// creating value for context
-	const value = {
-		enqueueSnackbar,
-		closeSnackbar: handleClose,
-	}
+    // creating value for context
+    const value = {
+        enqueueSnackbar,
+        closeSnackbar: handleClose,
+    }
 
-	return (
-		<SnackbarContext.Provider value={value}>
-			{props.children}
-			<Snackbar
-				open={isOpen}
-				onClose={handleClose}
-				TransitionComponent={SlideTransition}
-				{...state}
-				key={`open-snackbar-item-${currentSnackBarIndex}`}
-			// key={state.Transition.name}
-			/>
-		</SnackbarContext.Provider>
-	)
+    return (
+        <SnackbarContext.Provider value={value}>
+            {props.children}
+            <Snackbar
+                open={isOpen}
+                onClose={handleClose}
+                TransitionComponent={SlideTransition}
+                {...state}
+                key={`open-snackbar-item-${currentSnackBarIndex}`}
+                // key={state.Transition.name}
+            />
+        </SnackbarContext.Provider>
+    )
 }
 
 // creating custom hook to use snackbar
 const useSnackbar = () => {
-	const context = useContext(SnackbarContext)
-	if (context === undefined || !Object.keys(context).length)
-		throw new Error('useSnackbar must be used within a SnackbarContext')
+    const context = useContext(SnackbarContext)
+    if (context === undefined || !Object.keys(context).length)
+        throw new Error("useSnackbar must be used within a SnackbarContext")
 
-	return context
+    return context
 }
 
 export { SnackbarContextProvider }
