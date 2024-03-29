0e6796b5d530bb3a7a939fb549475221288deaa6
BiswajitDeka <utpaldeka102@gmail.com>
2024-03-29 12:25:01 +0530

    added icons and pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: { port: 3000 },
-	resolve: { alias: [{ find: '@', replacement: '/src' }] },
+    plugins: [
+        react(),
+        VitePWA({
+            includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+            manifest: {
+                name: "CardScore Tracker",
+                short_name: "CardScore Tracker",
+                description:
+                    "Your essential companion for effortless scorekeeping during card games, with real-time updates and customizable features.",
+                theme_color: "#ffc15e",
+                icons: [
+                    {
+                        src: "/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any",
+                    },
+                    {
+                        src: "/pwa-maskable-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                    {
+                        src: "/pwa-maskable-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+            },
+        }),
+    ],
+    server: { port: 3000, open: true },
+    resolve: { alias: [{ find: "@", replacement: "/src" }] },
 })
