260df6c424630ce20aecceec3abfed7904bcb68a
BiswajitDeka <utpaldeka102@gmail.com>
2024-03-29 21:56:34 +0530

    updated pwa

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,25 @@ export default defineConfig({
     plugins: [
         react(),
         VitePWA({
-            includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+            includeAssets: [
+                "favicon.ico",
+                "apple-touch-icon.png",
+                "mask-icon.png",
+            ],
             manifest: {
-                name: "CardScore Tracker",
-                short_name: "CardScore Tracker",
+                name: "Score Mate",
+                short_name: "Score Mate",
                 description:
                     "Your essential companion for effortless scorekeeping during card games, with real-time updates and customizable features.",
                 theme_color: "#ffc15e",
+                display: "standalone",
                 icons: [
+                    {
+                        src: "/apple-touch-icon.png",
+                        sizes: "180x180",
+                        type: "image/png",
+                        purpose: "apple touch icon",
+                    },
                     {
                         src: "/pwa-192x192.png",
                         sizes: "192x192",
@@ -38,6 +49,12 @@ export default defineConfig({
                         sizes: "512x512",
                         type: "image/png",
                         purpose: "maskable",
+                    },
+                    {
+                        src: "/mask-icon.png",
+                        sizes: "500x500",
+                        type: "image/png",
+                        purpose: "any maskable",
                     },
                 ],
             },
