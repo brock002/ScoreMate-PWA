72f455addbf1d351d542f852d1dc6fc689821c20
Biswajit Deka <51966542+brock002@users.noreply.github.com>
2024-03-31 11:31:03 +0530

    added footer and analytics (#6) (#7)

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 import ColorInput from "./ColorInput"
 import ConfirmationDialog from "./ConfirmationDialog"
+import Logo from "./Logo"
 import ResultsDialog from "./ResultsDialog"
 import StickyTableCell from "./StickyTableCell"
 
-export { ColorInput, ConfirmationDialog, ResultsDialog, StickyTableCell }
+export { ColorInput, ConfirmationDialog, Logo, ResultsDialog, StickyTableCell }
